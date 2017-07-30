ddccd1634e39e4bdaa700a7600e00cf2936586ff
Brandon Yee <brandon.yee150@gmail.com>
2017-07-30 00:22:08 -1000

    Added Mobile Hamburger

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,32 @@
 "use strict"; /* eslint-env browser */ /* global */ /* eslint no-warning-comments: [1, { "terms": ["todo", "fix", "help"], "location": "anywhere" }] */
 var socket = io.connect();
 
+document.addEventListener('DOMContentLoaded', function () {
+
+  // Get all "navbar-burger" elements
+  var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+  // Check if there are any nav burgers
+  if ($navbarBurgers.length > 0) {
+
+    // Add a click event on each of them
+    $navbarBurgers.forEach(function ($el) {
+      $el.addEventListener('click', () => {
+
+        // Get the target from the "data-target" attribute
+        var target = $el.dataset.target;
+        var $target = document.getElementById(target);
+
+        // Toggle the class on both the "navbar-burger" and the "navbar-menu"
+        $el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+
+      });
+    });
+  }
+
+});
+
 // Declare Variables
 var masterData = {};
 
@@ -19,7 +45,7 @@ Tabletop.init({
 
 			if ($(".dynamicHeader-" + identifier).length) {
 				$("section.hero.firstHero").parallax({
-					imageSrc: "https://drive.google.com/uc?export=view&id=" + data[i].imageURL.substring(32, 60),
+					imageSrc: data[i].imageURL,
 					speed: 0.2
 				});
 				$(".dynamicHeroBody-" + identifier).text(data[i].title);
